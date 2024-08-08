5e7ae3bbd0666c71d47d1f6a96b786ea24923c5e
OsnatBarnea <osnatbarnea1@gmail.com>
2024-08-08 14:11:49 +0300

    Nehorai-Yosef first version

---
+++ build/main.js
@@ -0,0 +1,49 @@
+//get elements:
+const switchImages = document.getElementById("switchImages");
+const donateSpan = document.getElementById("donateSpan");
+const donateDetails = document.getElementById("donateDetails");
+const video = document.getElementById("video");
+const article = document.getElementById("article");
+const imagesSrc = ["assets/images/Nehorai_1.jpg", "assets/images/Nehorai_2.jpg", "assets/images/Nehorai_3.jpg", "assets/images/Nehorai_4.jpg", "assets/images/Nehorai_5.jpg"];
+let index = 0;
+function switchImage() {
+    index = (++index) % imagesSrc.length;
+    switchImages.src = imagesSrc[index];
+}
+//switch image every 3 seconds
+setInterval(switchImage, 3000);
+const fontColor = ["#ff0000", "#ffffff"];
+function donationColor() {
+    index = (++index) % fontColor.length;
+    donateSpan.style.color = fontColor[index];
+}
+setInterval(donationColor, 500);
+//hover to see donation information:
+donateSpan.addEventListener("mouseenter", () => {
+    donateSpan.style.cursor = "pointer";
+    donateSpan.style.color = "Blue";
+    donateDetails.innerHTML = `<p>
+    לתרומה בביט 050-910-0003
+    <br>
+    להעברה בנקאית - שירה הראלי מס' חשבון 4610401
+    בנק הפועלים
+    סניף 698</p>`;
+});
+donateSpan.addEventListener("mouseleave", () => {
+    donateSpan.style.color = "";
+    donateDetails.innerHTML = "";
+});
+//the video starts when the page is uploaded
+window.onload = function () {
+    video.play();
+};
+article.addEventListener("click", () => {
+    window.open("https://www.rarediseaseadvisor.com/features/sarepta-looks-to-expand-new-gene-therapy-to-more-patients-with-dmd/", "_blank");
+});
+article.addEventListener("mouseenter", () => {
+    article.style.cursor = "pointer";
+    article.style.color = "Blue";
+});
+article.addEventListener("mouseleave", () => {
+    article.style.color = "";
+});
